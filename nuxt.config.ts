622d546d69d5c0c171467f0b3972e020a4e0cc2e
Ulyana <filkova.ulyana@wiseweb.by>
2025-12-10 17:59:40 +0300

    feat: add @nuxt/image library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@pinia/nuxt', '@nuxt/eslint'],
+  modules: ['@nuxt/ui', '@pinia/nuxt', '@nuxt/eslint', '@nuxt/image'],
   css: ['~/assets/css/main.css', '@/assets/css/theme.css', '@/assets/css/fonts.css',],
   ssr: true,
   nitro: {
